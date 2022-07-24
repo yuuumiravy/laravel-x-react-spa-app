25f4af0f0858fd8d99444e399e78dd20b17b596c
yuuumbk <hikoki.y@gmail.com>
2022-07-24 23:22:31 +0900

    create a factory for the schedules table

---
+++ database/factories/ScheduleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Schedule>
+ */
+class ScheduleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'date' => fake()->date(),
+            'time' => fake()->time(),
+            'category' => fake()->country(),
+            'content' => Str::random(10),
+        ];
+    }
+}
