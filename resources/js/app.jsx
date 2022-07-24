76a5a2cc19ff9e7aa1fc5fb00b1559f09d798d6e
yuuumbk <hikoki.y@gmail.com>
2022-07-24 17:51:55 +0900

    react install

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+import React from 'react';
+import { App } from './components/App';
+import { createRoot } from 'react-dom/client';
+
+const app = document.getElementById('app');
+const root = createRoot(app);
+root.render(<App />);
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    {{-- @viteReactRefresh --}}
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
