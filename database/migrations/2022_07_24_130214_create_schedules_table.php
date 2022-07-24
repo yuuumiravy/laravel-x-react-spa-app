96358147cbac1e66b1f536bc68a0724839bf3438
yuuumbk <hikoki.y@gmail.com>
2022-07-24 23:10:06 +0900

    create schedules table

--- database/migrations/2022_07_24_130214_create_schedules_table.php
+++ database/migrations/2022_07_24_130214_create_schedules_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('schedules', function (Blueprint $table) {
             $table->id();
+            $table->date('date');
+            $table->time('time');
+            $table->string('category');
+            $table->string('content');
             $table->timestamps();
         });
     }
